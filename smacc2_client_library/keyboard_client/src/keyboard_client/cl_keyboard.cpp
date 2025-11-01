d754bfcc09752038ce3a399baac27c5a169d6b20
brettpac <brett@robosoft.ai>
2025-11-01 03:48:42 -0700

    Rebase from 'upstream'

--- smacc2_client_library/keyboard_client/src/keyboard_client/cl_keyboard.cpp
+++ smacc2_client_library/keyboard_client/src/keyboard_client/cl_keyboard.cpp
@@ -12,31 +12,18 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+/*****************************************************************************************************************
+ *
+ * 	 Authors: Pablo Inigo Blasco, Brett Aldrich
+ *
+ ******************************************************************************************************************/
+
 #include <keyboard_client/cl_keyboard.hpp>
 
 namespace cl_keyboard
 {
-ClKeyboard::ClKeyboard()
-{
-  initialized_ = false;
-  topicName = "/keyboard_unicode";
-}
-
+// Declare the Client's default constructor.
+ClKeyboard::ClKeyboard() {}
+// Declare the Client's default destructor.
 ClKeyboard::~ClKeyboard() {}
-
-void ClKeyboard::onInitialize()
-{
-  SmaccSubscriberClient<std_msgs::msg::UInt16>::onInitialize();
-
-  if (!this->initialized_)
-  {
-    this->onMessageReceived(&ClKeyboard::onKeyboardMessage, this);
-    this->initialized_ = true;
-  }
-}
-
-void ClKeyboard::onKeyboardMessage(const std_msgs::msg::UInt16 & unicode_keychar)
-{
-  postEventKeyPress(unicode_keychar);
-}
 }  // namespace cl_keyboard
