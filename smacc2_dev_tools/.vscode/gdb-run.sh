a29729f3e1e0543abb6f24f626b04cc12f444ec1
brettpac <brett@robosoft.ai>
2026-05-31 22:49:10 -0700

    Trimmed the branch to only the smacc2_sm_reference_library/sm_atomic sub directory

--- smacc2_dev_tools/.vscode/gdb-run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-source /opt/ros/jazzy/setup.bash
-if [ -f /workspaces/isaac_ros-dev/install/setup.bash ]; then
-  source /workspaces/isaac_ros-dev/install/setup.bash
-fi
-exec "$@"
