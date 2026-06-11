fac320943343519da6c0faa4af40a5d8ceb7caaf
brettpac <brett@robosoft.ai>
2026-06-11 16:04:10 -0700

    Revert "Trimmed the branch to only the smacc2_sm_reference_library/sm_panda_cl_moveit2z_cb_inventory_isaacsim sub directory"
    This reverts commit 5cd0cf1a8683dbb9dce49c82f9cfd4a9b1e74b3c.

---
+++ smacc2_sm_reference_library/sm_three_some/include/sm_three_some/clients/cl_subscriber/client_behaviors/cb_default_subscriber_behavior.hpp
@@ -0,0 +1,40 @@
+// Copyright 2025 Robosoft Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include <sm_three_some/clients/cl_subscriber/cl_subscriber.hpp>
+#include <cl_generic_sensor/client_behaviors/cb_default_generic_sensor_behavior.hpp>
+
+namespace sm_three_some
+{
+namespace cl_subscriber
+{
+// Component-based default subscriber behavior
+// Uses the multirole sensor behavior for component-based architecture
+class CbDefaultSubscriberBehavior
+  : public cl_generic_sensor::CbDefaultGenericSensorBehavior<ClSubscriber>
+{
+public:
+  typedef std_msgs::msg::UInt16 TMessageType;
+
+  void onEntry() override
+  {
+    // Call base class onEntry to setup component connections
+    cl_generic_sensor::CbDefaultGenericSensorBehavior<ClSubscriber>::onEntry();
+
+    RCLCPP_INFO(getLogger(), "[CbDefaultSubscriberBehavior] Behavior active");
+  }
+};
+}  // namespace cl_subscriber
+}  // namespace sm_three_some
